54410b8d2feee61e20c34fef6d2c5af9f5cd7e80
Jose García <jose.garcia.escribano.bs@gmail.com>
2021-11-24 14:59:26 +0100

    Prueba terminada 80%

--- src/ArabicToRoman.php
+++ src/ArabicToRoman.php
@@ -4,19 +4,57 @@ namespace App;
 
 class ArabicToRoman
 {
+
+    private const ROMAN_VALUES = array(
+        "I" => 1,
+        'IV' => 4,
+        "V" => 5,
+        'IX' => 9,
+        "X" => 10,
+        'XL' => 40,
+        "L" => 50,
+        'XC' => 90,
+        "C" => 100,
+        'CD' => 400,
+        "D" => 500,
+        'CM' => 900,
+        "M" => 1000,
+    );
+
+
     /**
      * Receive an arabic number and return a string with its roman counterpart
      *
      * @param int $arabicNumber Arabic number to be transformed (e.g. 121)
      *
-     * @return string The roman number equivalent (e.g. CXXI)
+     * @return string|null The roman number equivalent (e.g. CXXI)
      */
-    public static function transform(int $arabicNumber): string
+    public static function transform(int $arabicNumber): string|null
     {
         $romanNumber = '';
 
-        // Complete the function
+        if ($arabicNumber > 0 && $arabicNumber <= 4000) {
+            // Si el valor consultado se encuentra ya en el array lo devolvemos directamente
+            $valueInArrayYet = array_search($arabicNumber, self::ROMAN_VALUES);
+            if ($valueInArrayYet !== false) {
+                return $valueInArrayYet;
+            }
+
+            while ($arabicNumber > 0) {
+                foreach (array_reverse(self::ROMAN_VALUES) as $letter => $value) {
+                    if($arabicNumber >= $value) {
+                        $arabicNumber -= $value;
+                        $romanNumber .= $letter;
+                        break;
+                    }
+                }
+            }
+
+        } else {
+            return null;
+        }
 
         return $romanNumber;
     }
+	
 }
--- src/Bakery.php
+++ src/Bakery.php
@@ -4,10 +4,11 @@ namespace App;
 
 class Bakery
 {
+
     /**
      * Calculate the output of cakes for a giver recipe
      *
-     * @param array $recipe      Contains the necessary ingredients to make one cake
+     * @param array $recipe Contains the necessary ingredients to make one cake
      * @param array $ingredients Contains the amount of ingredients you have available to bake
      *
      * @return int The number of cakes you can bake
@@ -15,9 +16,28 @@ class Bakery
     public static function calculateOutput(array $recipe, array $ingredients): int
     {
         $numberOfCakes = 0;
+        $enoughIngredientes = true;
 
-        // Complete the function
+        // Si falta algun ingrediente no se puede continuar y devolvemos 0
+        $diffBettewnArrays = array_diff_key($recipe, $ingredients);
+        if (!empty($diffBettewnArrays)) {
+            return 0;
+        }
+
+        while ($enoughIngredientes) {
+            foreach ($recipe as $key => $recipeValue) {
+                if ($ingredients[$key] >= $recipeValue) {
+                    $ingredients[$key] -= $recipeValue;
+                } else {
+                    $enoughIngredientes = false;
+                }
+            }
+            if ($enoughIngredientes) {
+                $numberOfCakes++;
+            }
+        }
 
         return $numberOfCakes;
     }
+	
 }
