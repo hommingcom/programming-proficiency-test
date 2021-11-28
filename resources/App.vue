a62c4a53308e5a14a898b27990952f6dd9c8ceb2
Jose García <jose.garcia.escribano.bs@gmail.com>
2021-11-28 01:17:39 +0100

    85% Falta la logica de los filtros de rentedFrom - rentedTo y estilos

--- resources/App.vue
+++ resources/App.vue
@@ -1,23 +1,76 @@
 <template>
   <div id="app">
-    <table v-if="properties !== undefined">
-      <tr>
-        <th>nº de referencia</th>
-        <th>Nombre del inmueble</th>
-        <th>Nombre del dueño</th>
-        <th>Tipo de vivienda</th>
-        <th>Propiedad disponible</th>
-        <th>Tiempo alquilado</th>
-      </tr>
-      <tr v-for="propertie in properties" :key="propertie.id">
-        <td>{{ propertie.id }}</td>
-        <td>{{propertie.name}}</td>
-        <td>{{getOwnerNameFromPropertie(propertie.userId)}}</td>
-        <td>{{getPropertieTypeFromId(propertie.typeId)}}</td>
-        <td>{{isPropertieCurrentlyRented(propertie)}}</td>
-        <td>{{diffBeteweenRentedDates(propertie)}}</td>
-      </tr>
-    </table>
+    <span>Filtro por usuario:</span>
+    <select v-model="inputUserIdFilterSelected" v-on:change="applyFilters()">
+      <option selected="selected" v-bind:value="null">Seleccione un elemento:</option>
+      <option v-for="user in users" :key="user.id" v-bind:value="user.id">{{ user.name }}</option>
+    </select>
+    <br>
+    <span>Filtro por tipo de vivienda:</span>
+    <select v-model="inputPropertieTypeIdFilterSelected" v-on:change="applyFilters()">
+      <option selected="selected" v-bind:value="null">Seleccione un elemento:</option>
+      <option v-for="propertieType in propertyTypes" :key="propertieType.id"
+              v-bind:value="propertieType.id">
+        {{ propertieType.name }}
+      </option>
+    </select>
+    <br>
+    <label>From</label>
+    <input type="date"
+           v-model="inputDateFromFilterSelected"
+           v-on:change="applyFilters()">
+    <br>
+    <label>To</label>
+    <input type="date"
+           v-bind:value="inputDateToFilterSelected"
+           v-on:change="applyFilters()">
+    <!--  FIN DE LOS INPUTS  -->
+    <div v-if="!isFilterSelected()">
+      <table>
+        <tr>
+          <th>nº de referencia</th>
+          <th>Nombre del inmueble</th>
+          <th>Nombre del dueño</th>
+          <th>Tipo de vivienda</th>
+          <th>Propiedad disponible</th>
+          <th>Tiempo alquilado</th>
+        </tr>
+        <tr v-for="propertie in properties" :key="propertie.id">
+          <td>{{ propertie.id }}</td>
+          <td>{{ propertie.name }}</td>
+          <td>{{ getOwnerNameFromPropertie(propertie.userId) }}</td>
+          <td>{{ getPropertieTypeFromId(propertie.typeId) }}</td>
+          <td>{{ isPropertieCurrentlyRented(propertie) }}</td>
+          <td>{{ diffBeteweenRentedDates(propertie) }}</td>
+        </tr>
+      </table>
+    </div>
+    <div v-else-if="propertiesFiltered.length > 0">
+      <table>
+        <tr>
+          <th>nº de referencia</th>
+          <th>Nombre del inmueble</th>
+          <th>Nombre del dueño</th>
+          <th>Tipo de vivienda</th>
+          <th>Propiedad disponible</th>
+          <th>Tiempo alquilado</th>
+        </tr>
+        <tr v-for="propertie in propertiesFiltered" :key="propertie.id">
+          <td>{{ propertie.id }}</td>
+          <td>{{ propertie.name }}</td>
+          <td>{{ getOwnerNameFromPropertie(propertie.userId) }}</td>
+          <td>{{ getPropertieTypeFromId(propertie.typeId) }}</td>
+          <td>{{ isPropertieCurrentlyRented(propertie) }}</td>
+          <td>{{ diffBeteweenRentedDates(propertie) }}</td>
+        </tr>
+      </table>
+    </div>
+    <div v-else-if="propertiesFiltered.length === 0 && isFilterSelected">
+      <p>No hay resultados para mostrar con el filtro de busqueda actual.</p>
+    </div>
+    <div v-else>
+      <p>Ha ocurrido un error inesperado.</p>
+    </div>
   </div>
 </template>
 
@@ -25,12 +78,23 @@
 import moment from 'moment';
 import { properties, propertyTypes, users } from '@/mocks/api';
 
+const inputUserIdFilterSelected = null;
+const inputPropertieTypeIdFilterSelected = null;
+const inputDateFromFilterSelected = null;
+const inputDateToFilterSelected = null;
+const propertiesFiltered = [];
+
 export default {
   name: 'App',
   data: () => ({
     users,
     propertyTypes,
     properties,
+    propertiesFiltered,
+    inputUserIdFilterSelected,
+    inputPropertieTypeIdFilterSelected,
+    inputDateFromFilterSelected,
+    inputDateToFilterSelected,
   }),
   methods: {
     getOwnerNameFromPropertie(userId) {
@@ -66,8 +130,76 @@ export default {
       }
       return '';
     },
-  },
-  mounted() {
+    isFilterSelected() {
+      return this.inputUserIdFilterSelected !== null
+        || this.inputPropertieTypeIdFilterSelected !== null
+        || this.inputDateFromFilterSelected !== null
+        || this.inputDateToFilterSelected !== null;
+    },
+    applyFilters() {
+      this.propertiesFiltered = [];
+      const filtersUsed = [];
+      if (this.inputDateFromFilterSelected === '') {
+        this.inputDateFromFilterSelected = null;
+      }
+      if (this.inputDateToFilterSelected === '') {
+        this.inputDateToFilterSelected = null;
+      }
+      if (this.inputUserIdFilterSelected !== null) {
+        filtersUsed.push({
+          name: 'userId',
+          value: this.inputUserIdFilterSelected,
+        });
+      }
+
+      if (this.inputPropertieTypeIdFilterSelected !== null) {
+        filtersUsed.push({
+          name: 'typeId',
+          value: this.inputPropertieTypeIdFilterSelected,
+        });
+      }
+
+      if (this.inputDateFromFilterSelected !== null) {
+        filtersUsed.push({
+          name: 'rentedFrom',
+          value: this.inputDateFromFilterSelected,
+        });
+      }
+
+      if (this.inputDateToFilterSelected !== null) {
+        filtersUsed.push({
+          name: 'rentedTo',
+          value: this.inputDateToFilterSelected,
+        });
+      }
+
+      if (this.isFilterSelected) {
+        this.propertiesFiltered = this.properties.filter((propertie) => {
+          let filterOkey = false;
+          filtersUsed.forEach((filter) => {
+            if (filter.name === 'userId') {
+              if (propertie.userId === filter.value) {
+                filterOkey = true;
+              }
+            } else if (filter.name === 'typeId') {
+              if (propertie.typeId === filter.value) {
+                filterOkey = true;
+              }
+            } else if (filter.name === 'rentedFrom') {
+              if (moment(propertie.rentedFrom) <= moment(filter.value)) {
+                filterOkey = true;
+              }
+            } else if (filter.name === 'rentedTo') {
+              if (moment(propertie.rentedTo) <= moment(filter.value)) {
+                filterOkey = true;
+              }
+            }
+            filterOkey = false;
+          });
+          return filterOkey;
+        });
+      }
+    },
   },
 };
 </script>
