1e56e3db75b1762a33c8284ed1746e448d069bd2
rojo5 <luis21197@gmail.com>
2022-02-13 00:36:48 +0100

    correcciones de CSS

--- resources/pages/home.vue
+++ resources/pages/home.vue
@@ -1,83 +1,74 @@
 <template>
-    <div class="container px-3 flex flex-col mx-auto">
-      <section class="mb-5 py-5 flex flex-col md:flex-row justify-around items-center">
-
-        <div class="mb-2 md:mb-0 ">
-          <div class="px-4 py-2 border-2 border-grey rounded-3xl relative">
-            <label class="absolute -my-4 -mx-2 tracking-wider
-            bg-white px-2 text-grey-darker">
-              Filtro Propietario
-            </label>
-            <input type="text" name="owner" id="inputFilterOwner"
-            class="appearance-none border-2 border-white rounded w-full py-2
-            text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-white"
-            placeholder="Propietario..."
-            v-model="filters.owner">
-          </div>
+  <div class="container px-3 flex flex-col mx-auto">
+    <section class="mb-5 px-3 flex flex-col md:flex-row md:items-center md:justify-around
+        space-y-5 md:space-y-0">
+      <div class="mb-2 md:mb-0">
+        <div class="px-4 py-2 border-2 border-grey rounded-3xl relative">
+          <label class="absolute -my-4 -mx-2 tracking-wider bg-white px-2 text-grey-darker">
+            Filtro Propietario
+          </label>
+          <input type="text" name="owner" class=" appearance-none border-2 border-white
+              rounded w-full py-2 text-grey-darker leading-tight focus:outline-none
+            focus:bg-white focus:border-white" placeholder="Propietario..."
+            v-model="filters.owner"/>
         </div>
+      </div>
 
-        <div class="mb-2 md:mb-0 ">
-          <div class="px-4 py-2 border-2 border-grey rounded-3xl relative">
-            <label class="absolute -my-4 -mx-2 tracking-wider
-            bg-white px-2 text-grey-darker">
-              Tipo
-            </label>
-            <select v-model="filters.type"
-            class="appearance-none border-2 border-white bg-white rounded w-full py-2
-            text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-white">
-              <option selected="selected" value="">Seleciona una opción</option>
-              <option v-for="type in propertyTypes" :key="type.id" :value="type.name">
-              {{type.name}}
-              </option>
-            </select>
-          </div>
+      <div class="mb-2 md:mb-0">
+        <div class="px-4 py-2 border-2 border-grey rounded-3xl relative">
+          <label class="absolute -my-4 -mx-2 tracking-wider bg-white px-2 text-grey-darker">
+            Tipo
+          </label>
+          <select v-model="filters.type" class="appearance-none border-2 border-white bg-white
+              rounded w-full py-2 text-grey-darker leading-tight focus:outline-none
+              focus:bg-white focus:border-white">
+            <option selected="selected" value="">Seleciona una opción</option>
+            <option v-for="type in propertyTypes" :key="type.id" :value="type.name">
+              {{ type.name }}
+            </option>
+          </select>
         </div>
+      </div>
 
-<div class="flex  flex-col md:flex-row">
-
+      <div class="flex flex-col md:flex-row">
         <div class="mb-2 md:mb-0">
           <div class="px-4 py-2 border-2 border-grey rounded-3xl relative">
-            <label class="absolute -my-4 -mx-2 tracking-wider
-            bg-white px-2 text-grey-darker">
+            <label class="absolute -my-4 -mx-2 tracking-wider bg-white px-2 text-grey-darker">
               Fecha inicio
             </label>
-            <input type="date" name="starDate" id="filterStartDate"
-            class="appearance-none border-2 border-white rounded w-full py-2 bg-white
-            text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-white"
-            placeholder="Fecha inicio..."
-            v-model="filters.rentedFrom">
+            <input type="date" name="starDate" class="appearance-none border-2 border-white
+                rounded w-full py-2 bg-white text-grey-darker leading-tight focus:outline-none
+              focus:bg-white focus:border-white" placeholder="Fecha inicio..."
+              v-model="filters.rentedFrom"/>
           </div>
         </div>
         <div class="mb-2 md:mb-0 md:ml-2">
           <div class="px-4 py-2 border-2 border-grey rounded-3xl relative">
-            <label class="absolute -my-4 -mx-2 tracking-wider
-            bg-white px-2 text-grey-darker">
+            <label class="absolute -my-4 -mx-2 tracking-wider bg-white px-2 text-grey-darker">
               Fecha fin
             </label>
-            <input type="date" name="endDate" id="filterEndDate"
-            class="appearance-none border-2 border-white rounded w-full py-2 bg-white
-            text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-white"
-            placeholder="Fecha final..."
-            v-model="filters.rentedTo">
+            <input type="date" name="endDate" class="appearance-none border-2 border-white rounded
+                w-full py-2 bg-white text-grey-darker leading-tight focus:outline-none
+              focus:bg-white focus:border-white" placeholder="Fecha final..."
+              v-model="filters.rentedTo"
+            />
           </div>
         </div>
-</div>
+      </div>
 
-<div class="">
-  <button
-  class="border-2 border-red-400 px-4 py-2 rounded-3xl hover:bg-red-500 hover:text-white
-  font-bold tracking-wider hover:cursor-pointer"
-  @click="resetFilters()">
-    Borrar filtros
-  </button>
-</div>
+      <div>
+        <button class=" border-2 border-red-400 px-4 py-2 rounded-3xl hover:bg-red-500
+        hover:text-white font-bold tracking-wider hover:cursor-pointer w-full"
+          @click="resetFilters()">
+          Borrar filtros
+        </button>
+      </div>
+    </section>
 
-      </section>
-
-      <section class="px-2">
-        <table-component :headers="headers"  :dataBody="filteredData"/>
-      </section>
-    </div>
+    <section class="px-2">
+      <table-component :headers="headers" :dataBody="filteredData" />
+    </section>
+  </div>
 </template>
 
 <script>
@@ -114,18 +105,22 @@ export default {
       let data = [...this.dataFormatted];
 
       if (this.filters.owner !== null && this.filters.owner !== '') {
-        data = data.filter(((item) => item.owner.toLowerCase()
-          .includes(this.filters.owner.toLowerCase())));
+        data = data.filter((item) => item.owner.toLowerCase()
+          .includes(this.filters.owner.toLowerCase()));
       }
       if (this.filters.type !== null && this.filters.type !== '') {
         data = data.filter((item) => item.type === this.filters.type);
       }
       if (this.filters.rentedFrom !== null && this.filters.rentedFrom !== '') {
-        data = data.filter((item) => item.rentedFrom > new Date(this.filters.rentedFrom));
+        data = data.filter(
+          (item) => item.rentedFrom > new Date(this.filters.rentedFrom),
+        );
       }
       if (this.filters.rentedTo !== null && this.filters.rentedTo !== '') {
         debugger;
-        data = data.filter((item) => item.rentedTo < new Date(this.filters.rentedTo));
+        data = data.filter(
+          (item) => item.rentedTo < new Date(this.filters.rentedTo),
+        );
       }
 
       return data;
@@ -148,7 +143,10 @@ export default {
     formatData(property) {
       const owner = this.getOwner(property.userId);
       const propertyType = this.getType(property.typeId);
-      const available = this.isAvailable(property.rentedFrom, property.rentedTo);
+      const available = this.isAvailable(
+        property.rentedFrom,
+        property.rentedTo,
+      );
 
       return {
         property: property.id,
@@ -210,5 +208,4 @@ export default {
 </script>
 
 <style>
-
 </style>
