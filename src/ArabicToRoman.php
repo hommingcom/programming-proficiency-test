df07c1d45259c453436d1f955a4d1c8ef840b62a
anamaria.cuevasgarcia <ana.bdr.7@gmail.com>
2023-03-07 21:26:35 +0100

    Prueba técnica

--- src/ArabicToRoman.php
+++ src/ArabicToRoman.php
@@ -15,8 +15,248 @@ class ArabicToRoman
     {
         $romanNumber = '';
 
-        // Complete the function
 
+        if($arabicNumber >= 1000 || $arabicNumber <= 0){
+            return 'el número no es válido';
+        }
+               
+        //comprobamos que el numero no tenga valor fijo
+        $romanNumber = $this->numbers($arabicNumber);
+
+
+        //si no tiene valor fijo
+        if($romanNumber == ''){
+            //separamos el numero en 1 posicion
+            $separarNumero = str_split($arabicNumber,1);      
+       
+            //recorremos el array
+            foreach($separarNumero as $key => $value){
+                //comprobamos que el numero no sea 0
+                if($value != 0){
+                    //hayamos la posicion del número
+                    $checkPosition = $this->checkPosition($key,$value,$separarNumero);
+
+
+                    $number = intval($checkPosition['number']);
+                    $position = $checkPosition['position'];
+               
+                    //comprobamos si el numero tiene valor fijo
+                    $letter = $this->numbers($number);
+                    
+                    //si no tiene valor fijo realizamos los calculos
+                    if($letter == ''){
+                        $letter = $this->checkNumber($number,$position);
+                    }
+
+                    //concatenamos el número
+                    $romanNumber = $romanNumber.$letter;
+                }
+               
+            }
+           
+        }
+        
         return $romanNumber;
     }
+
+   
+    /**
+     * Recibe un número árabico y comprueba si contiene valor
+     *
+     * @param int $number 
+     *
+     * @return string Número romano equivalente
+     */
+    public function numbers($number){
+
+
+        switch($number){
+            case 1:
+                $letter = 'I';
+                break;
+            case 5:
+                $letter = 'V';
+                break;
+            case 10:
+                $letter = 'X';
+                break;
+            case 50:
+                $letter = 'L';
+                break;
+            case 100:
+                $letter = 'C';
+                break;
+            case 500:
+                $letter = 'D';
+                break;
+            case 1000:
+                $letter = 'M';
+                break;  
+            default:
+                $letter = '';
+                break;
+        }
+
+
+        return $letter;
+    }
+
+
+    /**
+     * Recibe un numero y la posicion del numero
+     *
+     * @param int $number 
+     * 
+     * @param int $position
+     *
+     * @return string The roman number equivalent (e.g. CXXI)
+     */
+
+    public function checkNumber($number,$position){
+
+        //hayamos los valores de los números principales
+        $letrasPrincipales = $this->valuePosition($position);
+
+        //comprobamos si el número es menor al valor intermedio
+        if($number<$letrasPrincipales['intermediateValue']){
+           
+            //si el valor del número es menor de 1 del valor intermedio
+            if($number == ($letrasPrincipales['intermediateValue']-$letrasPrincipales['value'])){
+                //concatenamos la letra de menor valor con la letra intermedia
+                $letter = $letrasPrincipales['smallLetter'].$letrasPrincipales['middleLetter'];
+            }else{
+
+                $letter = '';              
+               
+                $counter = 0;
+                //realizamos un bucle no mas de 3 veces hasta hayar el numero 
+                for($i = 0;$i <=($letrasPrincipales['valueSmall']+3);$i++){
+                    //si el numero no es correcto, añadimos la letra de menor valor hasta que el numero sea correcto
+                    if($number == $counter){
+                        break;
+                    }else{
+                        $letter = $letter.$letrasPrincipales['smallLetter'];
+                        //controlamos que la suma se realize con el valor adecuado según la posición del número
+                        $counter = $counter+$letrasPrincipales['value'];
+                    }
+                }
+            }
+
+
+        }else{
+            //comprobamos si el valor del número es menor de 1 del valor mayor
+            if($number == ($letrasPrincipales['bigValue']-$letrasPrincipales['value'])){
+                //concatenamos la letra de menor valor con la letra mayor
+                $letter = $letrasPrincipales['smallLetter'].$letrasPrincipales['bigLetter'];
+            }else{
+                //comenzamos con la letra intermedia
+                $letter = $letrasPrincipales['middleLetter'];
+                //controlamos que el contador comienze con el valor intermedio
+                $counter = $letrasPrincipales['intermediateValue'];
+
+                //realizamos un bucle no mas de 3 veces hasta hayar el numero 
+                for($i = $letrasPrincipales['intermediateValue'];$i <=($letrasPrincipales['intermediateValue']+3);){
+                    //si el numero no es correcto, añadimos la letra de menor valor hasta que el numero sea correcto
+                    if($number == $counter){
+                        break;
+                    }else{
+                        $letter = $letter.$letrasPrincipales['smallLetter'];
+                        //controlamos que la suma se realize con el valor adecuado según la posición del número
+                        $counter = $contador+$letrasPrincipales['value'];
+                    }
+                }
+            }
+
+
+        }
+        return $letter;
+    }
+
+    /**
+     * Recibe la posicion del numero
+     *
+     * @param int $postion 
+     *
+     * @return array con los valores de los números según la posición
+     */
+
+    public function valuePosition($position){
+        switch($position){
+            case 2:
+                $array = [
+                    'smallLetter' => $this->numeros(1),
+                    'valueSmall' => 1,
+                    'middleLetter' => $this->numeros(5),
+                    'intermediateValue' => 5,
+                    'bigLetter' => $this->numeros(10),
+                    'bigValue' => 10,
+                    'value' => 1,
+                ];
+                break;
+            case 1:
+                $array = [
+                    'smallLetter' => $this->numeros(10),
+                    'valueSmall' => 10,
+                    'middleLetter' => $this->numeros(50),
+                    'intermediateValue' => 50,
+                    'bigLetter' => $this->numeros(100),
+                    'bigValue' => 100,
+                    'value' => 10,
+                ];
+                break;
+            case 0:
+                $array = [
+                    'smallLetter' => $this->numeros(100),
+                    'valueSmall' => 100,
+                    'middleLetter' => $this->numeros(500),
+                    'intermediateValue' => 500,
+                    'bigLetter' => $this->numeros(1000),
+                    'bigValue' => 1000,
+                    'value' => 100
+                ];
+                break;
+        }
+
+
+        return $array;
+    }
+
+    /**
+     * Comprueba la posicion del numero
+     *
+     * @param int $key
+     * 
+     * @param int $number
+     * 
+     * @param int $separarNumero 
+     *
+     * @return array La posicion y el número
+     */
+
+
+    public function checkPosition($key,$number,$separarNumero){
+
+
+        if(count($separarNumero)==2 && $key == 0){
+            $number = $number*10;
+        }elseif(count($separarNumero)==3 && $key == 0){
+            $number = $number*100;
+        }elseif(count($separarNumero)==3 && $key == 1){
+            $number = $number * 10;
+        }
+       
+        if((($number%1000-$number%100)/100) > 0 ){
+            return  ['position'=>0,'number'=>$number];
+        }elseif((($number%100-$number%10)/10) > 0){
+            return ['position' => 1,'number'=> $number];
+        }elseif(($number%10) >0){
+            return ['position' => 2,'number' => $number];
+        }else{
+            return['position'=>2,'number' => $number];
+        }
+       
+    }
+
+
 }
+?>
--- src/Bakery.php
+++ src/Bakery.php
@@ -14,10 +14,58 @@ class Bakery
      */
     public static function calculateOutput(array $recipe, array $ingredients): int
     {
+
+
+        // $recipe = [
+        //     'ingrediente 1' => 500,
+        //     'ingrediente 2' => 236,
+        //     'ingrediente 3' => 125,
+        //     'ingrediente 4' => 656,
+        //     'ingrediente 5' => 335
+        // ];
+
+
+        // $ingredients = [
+        //     'ingrediente 1' => 12500,
+        //     'ingrediente 2' => 13236,
+        //     'ingrediente 3' => 12125,
+        //     'ingrediente 4' => 12656,
+        //     'ingrediente 5' => 1400
+        // ];
+
         $numberOfCakes = 0;
 
-        // Complete the function
+        do{
+            //comprueba que todos los ingredientes tienen cantidad suficiente para realizar la receta
+            $ingredients = $this->checkIngredients($recipe,$ingredients);
+            if($ingredients != false) $numberOfCakes++;              
+           
+        }while($ingredients != false);
+       
 
         return $numberOfCakes;
     }
+
+    
+
+    public function checkIngredients($recipe,$ingredients){
+        //recorre el array de ingredientes
+        foreach($ingredients as $key => $value){
+            if($recipe[$key] <= $value){
+                //si hay suficiente cantidad, resta la cantidad que se ha utilizado para realizar la tarea
+                $ingredients[$key] = $value - $recipe[$key];
+            }else{
+                //si no hay cantidad suficiente, retorna falso.
+                return false;
+            }
+        }
+
+
+        return $ingredients;
+
+
+    }
+
+
 }
+?>
