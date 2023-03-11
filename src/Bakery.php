46a8ab56c9c89a94466d4feac5d58a57410e3e16
SoulApps <ramon.guardialopez@gmail.com>
2023-03-11 15:23:01 +0100

    Optimizing code

--- src/Bakery.php
+++ src/Bakery.php
@@ -5,7 +5,7 @@ namespace App;
 class Bakery
 {
     /**
-     * Calculate the output of cakes for a giver recipe
+     * Calculate the output of cakes for a given recipe
      *
      * @param array $recipe      Contains the necessary ingredients to make one cake
      * @param array $ingredients Contains the amount of ingredients you have available to bake
@@ -14,23 +14,18 @@ class Bakery
      */
     public static function calculateOutput(array $recipe, array $ingredients): int
     {
-        $numberOfCakes = 0;
-        $maxCakes = INF;
+        $numberOfCakes = PHP_INT_MAX;
+        $missingIngredients = array_diff_key($recipe, $ingredients);
 
-        $availableIngredients = [];
-
-        foreach ($ingredients as $ingredient => $amount) {
-            $availableIngredients[$ingredient] = $amount;
+        if (!empty($missingIngredients)) {
+            return 0;
         }
 
         foreach ($recipe as $ingredient => $amount) {
-            if (!isset($availableIngredients[$ingredient]) || $availableIngredients[$ingredient] < $amount) {
-                $maxCakes = 0;
-                break;
-            }
-
-            $maxCakes = min($maxCakes, intdiv($availableIngredients[$ingredient], $amount));
+            $numberOfCakes = min($numberOfCakes, intdiv($ingredients[$ingredient], $amount));
         }
-        return $maxCakes;
+
+        return $numberOfCakes;
     }
+
 }
