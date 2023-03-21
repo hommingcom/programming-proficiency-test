275f02a75ef833b9ef973ecef7b43ce5f4bd51b2
alex fernandez <alexferfre@hotmail.com>
2023-03-21 20:56:37 +0100

    add: function ArabicToRoman

--- src/ArabicToRoman.php
+++ src/ArabicToRoman.php
@@ -11,11 +11,35 @@ class ArabicToRoman
      *
      * @return string The roman number equivalent (e.g. CXXI)
      */
-    public static function transform(int $arabicNumber): string
+    public static function transform(int $arabicNumber): string|null
     {
-        $romanNumber = '';
+        if ($arabicNumber <= 0) {
+            return null; //return null when we get a 0 or negative number
+        }
 
-        // Complete the function
+        $romanNumber = '';
+        $romanNumbers = array(
+            'M' => 1000,
+            'CM' => 900,
+            'D' => 500,
+            'CD' => 400,
+            'C' => 100,
+            'XC' => 90,
+            'L' => 50,
+            'XL' => 40,
+            'X' => 10,
+            'IX' => 9,
+            'V' => 5,
+            'IV' => 4,
+            'I' => 1
+        );
+
+        foreach ($romanNumbers as $letter => $value) {
+            while ($arabicNumber >= $value) {
+                $romanNumber .= $letter;
+                $arabicNumber -= $value;
+            }
+        }
 
         return $romanNumber;
     }
