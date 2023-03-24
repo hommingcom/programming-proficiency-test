0a9b3a2b6744a05835026841d611dd27c918836c
javirealprieto <javi.real.prieto@gmail.com>
2023-03-24 15:58:53 +0100

    backend: finish arabicToRoman function transform

--- src/ArabicToRoman.php
+++ src/ArabicToRoman.php
@@ -15,8 +15,32 @@ class ArabicToRoman
     {
         $romanNumber = '';
 
-        // Complete the function
-
+        $romanNumerals = array(
+            'M' => 1000,
+            'CM' => 900,
+            'D' => 500,
+            'CD' => 400,
+            'C' => 100,
+            'XC' => 90,
+            'L' => 50,
+            'XL' => 40,
+            'X' => 10,
+            'IX' => 9,
+            'V' => 5,
+            'IV' => 4,
+            'I' => 1
+        );
+        
+        // Iterate through the Roman numerals in descending order of their value
+        foreach ($romanNumerals as $arabic => $roman) {
+            // if the input number is >= to the $arabicNumber,
+            // subtract the $arabicNumber from the input number and add the corresponding Roman numeral to the $romanNumber
+            while ($arabicNumber >= $arabic) {
+            $romanNumber .= $roman;
+            $arabicNumber -= $arabic;
+            }
+        }
         return $romanNumber;
+
     }
 }
