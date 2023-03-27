e9a55ac82c88c6987d21ee60a63f62eb2dc5f312
Jose Pascual <josepascual92@gmail.com>
2023-03-27 17:51:34 +0200

    Frontend Tests

--- resources/App.vue
+++ resources/App.vue
@@ -1,18 +1,108 @@
 <template>
-  <div id="app">
-    WORK HERE
+  <div id="app" class="container">
+    <div class="container-fluid">
+      <form @submit.prevent="filterPropertiesByCriteria()">
+        <label>User:</label>
+        <select v-model.number="userIdSelected" data-userid>
+          <option v-for="user in users" :key="user.id" :value="user.id">
+            {{ user.name }}
+          </option>
+        </select>
+        <label>Property Type:</label>
+        <select v-model.number="propertyTypeIdSelected" data-typeid>
+          <option v-for="type in propertyTypes" :key="type.id" :value="type.id">
+            {{ type.name }}
+          </option>
+        </select>
+        <label>Rented From</label>
+        <input type="date" v-model="propertyRentedFrom" data-rentedfrom/>
+        <label>Rented To</label>
+        <input type="date" v-model="propertyRentedTo" data-rentedto/>
+        <button type="submit" class="btn btn-primary">Search</button>
+      </form>
+    </div>
+    <div>
+      <table class="table table-bordered table-striped">
+        <thead>
+        <tr>
+          <th>#</th>
+          <th>User</th>
+          <th>Type</th>
+          <th>Name</th>
+          <th>From</th>
+          <th>To</th>
+          <th>Renting</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="property in properties" :key="property.id">
+          <th>{{ property.id }}</th>
+          <td>{{ getUser(property.userId) }}</td>
+          <td>{{ getPropertyType(property.typeId) }}</td>
+          <td>{{ property.name }}</td>
+          <td>{{ formatDateToString(property.rentedFrom) }}</td>
+          <td>{{ formatDateToString(property.rentedTo) }}</td>
+          <td>{{ castBooleanToHuman(property.isRenting) }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-import { users, propertyTypes, properties } from '@/mocks/api';
+import usersRepository from '@/repositories/usersRepository';
+import propertyTypesRepository from '@/repositories/propertyTypesRepository';
+import propertiesRepository from '@/repositories/propertiesRepository';
 
 export default {
   name: 'App',
   data: () => ({
-    users,
-    propertyTypes,
-    properties,
+    users: [],
+    properties: [],
+    propertyTypes: [],
+    userIdSelected: null,
+    propertyTypeIdSelected: null,
+    propertyRentedFrom: null,
+    propertyRentedTo: null,
   }),
+  methods: {
+    getAllUsers() {
+      this.users = usersRepository.getAll();
+    },
+    getUser(userId) {
+      const users = this.users.filter((item) => item.id === userId);
+      return users[0].name;
+    },
+    getAllProperties() {
+      this.properties = propertiesRepository.getAll();
+    },
+    getAllPropertyTypes() {
+      this.propertyTypes = propertyTypesRepository.getAll();
+    },
+    getPropertyType(typeId) {
+      const types = this.propertyTypes.filter((item) => item.id === typeId);
+      return types[0].name;
+    },
+    castBooleanToHuman(value) {
+      return value ? 'Yes' : 'No';
+    },
+    formatDateToString(date) {
+      return date ? date.toLocaleDateString('es-ES') : '';
+    },
+    filterPropertiesByCriteria() {
+      this.properties = propertiesRepository.filterByCriteria({
+        userId: this.userIdSelected,
+        typeId: this.propertyTypeIdSelected,
+        rentedFrom: this.propertyRentedFrom ? new Date(this.propertyRentedFrom) : null,
+        rentedTo: this.propertyRentedTo ? new Date(this.propertyRentedTo) : null,
+      });
+    },
+  },
+  mounted() {
+    this.getAllUsers();
+    this.getAllPropertyTypes();
+    this.getAllProperties();
+  },
 };
 </script>
--- resources/main.js
+++ resources/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false;
 
