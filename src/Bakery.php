55845cb317c73f56e27b5ce5e0013bfde88a68e0
alin-galben <alin98g@gmail.com>
2023-04-05 18:55:57 +0200

    Solución prueba técnica

--- src/Bakery.php
+++ src/Bakery.php
@@ -16,7 +16,24 @@ class Bakery
     {
         $numberOfCakes = 0;
 
-        // Complete the function
+        // Loop through each ingredient in the recipe
+        foreach ($recipe as $ingredient => $quantity){
+            // Check if the ingredient is missing or there isnt enough of it
+            if(!isset($ingredients[$ingredient]) || $ingredients[$ingredient] < $quantity){
+                // If so, we cant make any cakes so we return 0
+                return 0;
+            }
+
+            // Calculate how many cakes we can make with the current ingredient
+            $cakesPerIngredient = intval($ingredients[$ingredient] / $quantity);
+
+            /** If we havent set $numberOfCakes yet or if we can make fewer cakes 
+             *  with the current ingredient than the previus one, update $numberOfCakes
+            */ 
+            if ($numberOfCakes === 0 || $cakesPerIngredient < $numberOfCakes) {
+                $numberOfCakes = $cakesPerIngredient;
+            }
+        }
 
         return $numberOfCakes;
     }
