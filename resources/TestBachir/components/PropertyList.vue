fe6831af101e472299430495ee928327b7120c7f
Bachir93 <bachirhassani@gmail.com>
2023-05-04 09:10:47 +0200

    cambio

--- resources/TestBachir/components/PropertyList.vue
+++ resources/TestBachir/components/PropertyList.vue
@@ -1,8 +1,28 @@
 <template>
   <div>
-    <div v-for="propiedad in propiedades" :key="propiedad.id">
-      <h3>{{ propiedad.name }}</h3>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>UserId</th>
+          <th>TypeId</th>
+          <th>Name</th>
+          <th>RentedFrom</th>
+          <th>RentedTo</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(propiedad, indice) of propiedades" :key="indice">
+          <td>{{ propiedad.id }}</td>
+          <td>{{ propiedad.userId }}</td>
+          <td>{{ propiedad.typeId }}</td>
+          <td>{{ propiedad.name }}</td>
+          <td>{{ propiedad.rentedFrom }}</td>
+          <td>{{ propiedad.rentedTo }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -14,14 +34,31 @@ export default {
       propiedades: [],
     };
   },
-  /* Una vez se crea la instancia de vue, el created me ejecutaría el codigo que tenga dentro */
+  /* Una vez se crea la instancia de vue, el created me ejecutaría el código que tenga dentro */
   async created() {
     /* Hasta que no se resuelva el await, no va a continuar con la ejecucion del codigo */
     const response = await fetch('@/mocks/api.js');
-    /* Nos devuelve un objeto de tipo response */
-    const propiedadesData = await response.json();
-    /* Quiero que me vaya guardando cada objeto en el array */
-    this.propiedades = propiedadesData;
+    if (response.ok) {
+      /* Nos devuelve un objeto de tipo response */
+      const propiedadesData = await response.json();
+      /* Quiero que me vaya guardando cada objeto en el array */
+      this.propiedades = propiedadesData;
+      console.log(response);
+    } else {
+      alert(`Error Desconocido${response.status}`);
+    }
   },
 };
 </script>
+
+<style>
+table{
+  width: 90%;
+  border: 1px solid black;
+  margin: auto;
+}
+th{
+  margin: 1rem;
+  background-color: blueviolet;
+}
+</style>
