b104d909276abb6248ecaee6401d24f2620337f0
David Hermo Blanco <155154557+david-eagora@users.noreply.github.com>
2025-01-09 22:26:47 +0100

    Refactor(bakery): enhance robustness

--- src/ArabicToRoman.php
+++ src/ArabicToRoman.php
@@ -4,6 +4,22 @@ namespace App;
 
 class ArabicToRoman
 {
+    public const array NUMBER_MAP = [
+        1000 => 'M',
+        900 => 'CM',
+        500 => 'D',
+        400 => 'CD',
+        100 => 'C',
+        90 => 'XC',
+        50 => 'L',
+        40 => 'XL',
+        10 => 'X',
+        9 => 'IX',
+        5 => 'V',
+        4 => 'IV',
+        1 => 'I',
+    ];
+
     /**
      * Receive an arabic number and return a string with its roman counterpart
      *
@@ -11,12 +27,22 @@ class ArabicToRoman
      *
      * @return string The roman number equivalent (e.g. CXXI)
      */
-    public static function transform(int $arabicNumber): string
+    public static function transform(int $arabicNumber): ?string
     {
+        if ($arabicNumber <= 0) {
+            return null;
+        }
+
         $romanNumber = '';
 
-        // Complete the function
+        foreach (self::NUMBER_MAP as $value => $symbol) {
+            while ($arabicNumber >= $value) {
+                $romanNumber .= $symbol;
+                $arabicNumber -= $value;
+            }
+        }
 
         return $romanNumber;
     }
+
 }
