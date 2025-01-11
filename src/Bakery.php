384f634f24941e9ec4c666f9fc9709ed49758757
Enrique Leon <enriqueleonq1@gmail.com>
2025-01-11 18:06:06 -0400

    refactor: adding logic to calculate the output of cakes for a giver recipe

--- src/Bakery.php
+++ src/Bakery.php
@@ -13,10 +13,19 @@ class Bakery
      * @return int The number of cakes you can bake
      */
     public static function calculateOutput(array $recipe, array $ingredients): int
-    {
+    {  
+        if(count($recipe) == 0 || count($ingredients) == 0)
+            return 0;
+
         $numberOfCakes = 0;
 
-        // Complete the function
+        foreach($recipe as $ingredient => $amountNeeded) {
+            if(!isset($ingredients[$ingredient]))
+                return 0;
+            $cakesPossible = intdiv($ingredients[$ingredient],$amountNeeded);
+            if($numberOfCakes == 0 || $cakesPossible < $numberOfCakes)
+                $numberOfCakes = $cakesPossible;
+        }
 
         return $numberOfCakes;
     }
